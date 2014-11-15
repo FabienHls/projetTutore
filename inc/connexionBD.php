d9be2b0a5f9703afa89c86a0eb068ea502d045e6
FabienHls <fhaureils@gmail.com>
2014-11-15 15:47:53 +0100

    Update connexionBD.php

--- inc/connexionBD.php
+++ inc/connexionBD.php
@@ -1,10 +1,12 @@
 <?php
-$serveur = "localhost";
-$nom_base = "raidSportif";
-$login = "root";
-$motdepasse = "root";
+$db=mysqli_connect('localhost','root','root','raidSportif');
+
+//$serveur = "localhost";
+//$nom_base = "raidSportif";
+//$login = "root";
+//$motdepasse = "root";
 // connexion à MySQL
-$connexion = mysql_connect ($serveur,$login,$motdepasse) or die ('ERREUR '.mysql_error());
+//$connexion = mysql_connect ($serveur,$login,$motdepasse) or die ('ERREUR '.mysql_error());
 // sélection de la base de données
-mysql_select_db ($nom_base,$connexion) or die ('ERREUR '.mysql_error());
+//mysql_select_db ($nom_base,$connexion) or die ('ERREUR '.mysql_error());
 ?>
