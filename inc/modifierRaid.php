6d0935226e4ebfbab3939fe32dcd590792de1faa
FabienHls <FabienHls@users.noreply.github.com>
2014-11-17 13:09:42 +0100

    Update modifierRaid.php

--- inc/modifierRaid.php
+++ inc/modifierRaid.php
@@ -12,25 +12,30 @@ if (isset($_GET['nomEtape'])) {
 	$results = mysqli_query($db,$sql);
 	$nbResults=mysqli_num_rows($result); // Nombre de résultats
 	$row=mysqli_fetch_assoc($result);
-}
-// If there are no records.
-if($nbResults==0) {  // Si 0 resultat
-	echo "<tr>";
-		echo "<td colspan='4'>There were not records</td>";
-	echo "</tr>";
-}
-else {
-	foreach ($rows as $row) {
+
+	// If there are no records.
+	if($nbResults==0) {  // Si 0 resultat
 		echo "<tr>";
-			echo "<td>".$row['nomEtape']."</td>";
-			echo "<td>".$row['typeEtape']."</td>";
-			echo "<td>".$row['dateDebutEtape']."</td>";
-			echo "<td>".$row['dateFinEtape']."</td>";
-			echo "<td>".$row['heureDepart']."</td>";
-			echo "<td>".$row['heureFin']."</td>";
-			echo "<td>".$row['lieuDepart']."</td>";
-			echo "<td>".$row['lieuArrivee']."</td>";
+			echo "<td colspan='4'>There were not records</td>";
 		echo "</tr>";
 	}
+	else {
+		foreach ($rows as $row) {
+			echo "<tr>";
+				echo "<td>".$row['nomEtape']."</td>";
+				echo "<td>".$row['typeEtape']."</td>";
+				echo "<td>".$row['dateDebutEtape']."</td>";
+				echo "<td>".$row['dateFinEtape']."</td>";
+				echo "<td>".$row['heureDepart']."</td>";
+				echo "<td>".$row['heureFin']."</td>";
+				echo "<td>".$row['lieuDepart']."</td>";
+				echo "<td>".$row['lieuArrivee']."</td>";
+			echo "</tr>";
+		}
+	}	
 }
+else{
+	echo "Rien de passe en GET";
+}
+
 ?>
