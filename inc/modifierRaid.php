633a674ea74d2e930ed3ac1827cb3ad59a281f63
FabienHls <FabienHls@users.noreply.github.com>
2014-11-17 13:13:46 +0100

    Update modifierRaid.php

--- inc/modifierRaid.php
+++ inc/modifierRaid.php
@@ -8,7 +8,7 @@ if (isset($_GET['nomEtape'])) {
 	//$sql ="SELECT * FROM Etape WHERE nomEtape= '%s'";
 	//$query=sprintf($sql, $data);
 	$sql="SELECT * FROM Etape WHERE nomEtape=".$_GET['nomEtape'];
-	echo $sql;
+	echo "samuel";
 	$results = mysqli_query($db,$sql);
 	$nbResults=mysqli_num_rows($results); // Nombre de résultats
 	$rows=mysqli_fetch_assoc($results);
